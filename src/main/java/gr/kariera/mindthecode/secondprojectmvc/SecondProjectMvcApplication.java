44bf9db544a92a0f9b9f319dfac6d63d526007d8
Panagiotis Liaskos <pnliaskos@gmail.com>
2023-03-17 20:27:49 +0200

    Creation of new Project

---
+++ src/main/java/gr/kariera/mindthecode/secondprojectmvc/SecondProjectMvcApplication.java
@@ -0,0 +1,13 @@
+package gr.kariera.mindthecode.secondprojectmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondProjectMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondProjectMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gr/kariera/mindthecode/secondprojectmvc/SecondProjectMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.kariera.mindthecode.secondprojectmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondProjectMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
