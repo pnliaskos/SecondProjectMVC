15624606dd44d608eb3665c2e99075ce2a5349a1
asfy <ang.loudn@gmail.com>
2023-03-20 17:40:14 +0200

    V1

---
+++ src/main/java/gr/kariera/mindthecode/secondprojectmvc/DTO/ProductWithQuantityExtendedDto.java
@@ -0,0 +1,25 @@
+package gr.kariera.mindthecode.secondprojectmvc.DTO;
+
+import java.math.BigDecimal;
+
+public class ProductWithQuantityExtendedDto extends ProductWithQuantityDto {
+    private BigDecimal price;
+
+    private String description;
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/main/java/gr/kariera/mindthecode/secondprojectmvc/DTO/Test.java
@@ -0,0 +1,4 @@
+package gr.kariera.mindthecode.secondprojectmvc.DTO;
+
+public class Test {
+}
