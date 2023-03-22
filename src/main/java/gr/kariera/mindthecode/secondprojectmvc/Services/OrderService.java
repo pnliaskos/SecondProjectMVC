17e3b40ec2cd8295e25f6ffa6f283806d1622e96
asfy <ang.loudn@gmail.com>
2023-03-22 17:22:50 +0200

    Added OrderService

--- src/main/java/gr/kariera/mindthecode/secondprojectmvc/Services/OrderService.java
+++ src/main/java/gr/kariera/mindthecode/secondprojectmvc/Services/OrderService.java
@@ -1,2 +1,18 @@
-package gr.kariera.mindthecode.secondprojectmvc.Services;public interface OrderService {
+package gr.kariera.mindthecode.secondprojectmvc.Services;
+
+import gr.kariera.mindthecode.secondprojectmvc.DTO.NewOrderDto;
+import gr.kariera.mindthecode.secondprojectmvc.Entities.Order;
+import org.springframework.data.domain.Page;
+
+
+public interface OrderService {
+    Order getOrderById(Integer id);
+
+    Order createNewOrder(NewOrderDto newOrder);
+
+    Page<Order> getAllOrders(String address, int page, int size, String sort);
+
+    void updateOrder(Integer id, Order order);
+
+    void deleteOrder(Integer id);
 }
